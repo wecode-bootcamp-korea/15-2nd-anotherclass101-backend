8372ae8e0917793a42336cc46261372aab620560
mincheolkk <kmc04080408@gmail.com>
2020-12-28 22:33:14 +0900

    Fix: 초기세팅수정

--- ac101/settings.py
+++ ac101/settings.py
@@ -38,18 +38,18 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'corsheaders’
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-   # 'django.middleware.common.CommonMiddleware',
+    'django.middleware.common.CommonMiddleware',
    # 'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
+   # 'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    ‘corsheaders.middleware.CorsMiddleware’,
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'ac101.urls'
@@ -117,38 +117,38 @@ APPEND_SLASH = False
 CORS_ORIGIN_ALLOW_ALL=True
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_METHODS = (
-    ‘DELETE’,
-    ‘GET’,
-    ‘OPTIONS’,
-    ‘PATCH’,
-    ‘POST’,
-    ‘PUT’,
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
 )
 CORS_ALLOW_HEADERS = (
-    ‘accept’,
-    ‘accept-encoding’,
-    ‘authorization’,
-    ‘content-type’,
-    ‘dnt’,
-    ‘origin’,
-    ‘user-agent’,
-    ‘x-csrftoken’,
-    ‘x-requested-with’,
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
 )
 LOGGING = {
-    ‘disable_existing_loggers’: False,
-    ‘version’: 1,
-    ‘handlers’: {
-        ‘console’: {
-            ‘class’: ‘logging.StreamHandler’,
-            ‘level’: ‘DEBUG’,
+    'disable_existing_loggers': False,
+    'version': 1,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'level': 'DEBUG',
         },
     },
-    ‘loggers’: {
-        ‘django.db.backends’: {
-            ‘handlers’: [‘console’],
-            ‘level’: ‘DEBUG’,
-            ‘propagate’: False,
+    'loggers': {
+        'django.db.backends': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': False,
         },
     },
 }
